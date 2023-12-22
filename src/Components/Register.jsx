9c85f5d5ac1070c21a97739ec50763f5d77319f3
yashm13114 <yashm13114@gmail.com>
2023-12-21 19:38:47 -0500

    add url

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -56,7 +56,7 @@ const Register = () => {
                 })
             } else {
 
-                const res = await fetch('http://localhost:5000/register', {
+                const res = await fetch('https://server-yash.onrender.com/register', {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
