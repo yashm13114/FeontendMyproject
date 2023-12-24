4df47619bd4407d5000c350297ba82c6f7081ffa
yashm13114 <yashm13114@gmail.com>
2023-12-24 11:35:18 -0500

    loader update

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -3,7 +3,7 @@ import './loader.css'
 function Loader() {
     return (
         <div className="loader-container">
-            <div className="loader">loading...</div>
+            <div className="loader"></div>
         </div>
     )
 }
